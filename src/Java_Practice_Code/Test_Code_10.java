6df0d55f4e74972013b42d875dba2e1423251e8c
Ahmad11-12git <ahmadk@safexpay.com>
2024-07-25 16:03:27 +0530

    Adding new code for the switch case

---
+++ src/Java_Practice_Code/Test_Code_10.java
@@ -0,0 +1,23 @@
+package Java_Practice_Code;
+
+import java.util.Scanner;
+
+public class Test_Code_10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter yor Name");
+        String Name = sc.nextLine();
+        System.out.println("Enter Yor Salary");
+        int salary = sc.nextInt();
+        System.out.println("Enter yor Edge");
+        int edge = sc.nextInt();
+
+        if (salary <= 300000)
+        {
+            System.out.println("Hi "+ Name + " Yor are not eligible for the tax payer");
+        }else {
+            System.out.println("Hi "+ Name + " You are eligble for the tax paying");
+        }
+        System.out.println("Hi " + Name + " Yor edge is " + edge );
+    }
+}
--- src/Java_Practice_Code/Test_Code_9.java
+++ src/Java_Practice_Code/Test_Code_9.java
@@ -1,7 +1,39 @@
 package Java_Practice_Code;
 
+import java.util.Scanner;
+
 public class Test_Code_9 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Day Name");
+        int dayOfWeek = sc.nextInt();
+        String dayName;
+        switch (dayOfWeek){
+            case 1:
+                dayName = "Monday";
+                break;
+            case 2:
+                dayName = "Tuesday";
+                break;
+            case 3:
+                dayName = "Wednesday";
+                break;
+            case 4:
+                dayName = "Thursday";
+                break;
+            case 5:
+                dayName = "Friday";
+                break;
+            case 6:
+                dayName = "Saturday";
+                break;
+            case 7:
+                dayName = "Sunday";
+                break;
+            default:
+                dayName = "Invalid day";
+                break;
+        }
+        System.out.println("Day "+ dayOfWeek + " is " + dayName);
     }
 }
